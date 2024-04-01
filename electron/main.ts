b5b62741748c38d674b994506deecd8ed0bb2175
xNTFVx <135262384+xNTFx@users.noreply.github.com>
2024-04-01 21:41:18 +0200

    Update main.ts

--- electron/main.ts
+++ electron/main.ts
@@ -38,6 +38,8 @@ function createWindow() {
     },
   });
 
+  nativeTheme.themeSource = "dark";
+
   // Test active push message to Renderer-process.
   win.webContents.on('did-finish-load', () => {
     win?.webContents.send('main-process-message', new Date().toLocaleString());
